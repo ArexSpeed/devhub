6388388196e5d4a867ac944850e264a7548d8778
Arek <arexspeed95@gmail.com>
2022-03-23 14:25:32 +0100

    community tutorial

---
+++ components/DevCard.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import { DrupalIcon, JavaScriptIcon, ReactIcon } from './Icons'
+
+const DevCard = ({id, name, job}) => {
+  return (
+    <div className="devcard">
+      <article className="devcard__top">
+        <div className="devcard__image"></div>
+        <div className="devcard__details">
+          <div className="devcard__details--langs">PL, ENG, GER</div>
+          <div className="devcard__details--name">{name}</div>
+          <div className="devcard__details--title">{job}</div>
+        </div>
+      </article>
+      <div className="devcard__smalltext">Main skills</div>
+      <article className="devcard__skills">
+        <ReactIcon className="icon-medium primary-blue" />
+        <JavaScriptIcon className="icon-medium primary-blue" />
+        <DrupalIcon className="icon-medium primary-blue" />
+      </article>
+      <article className="devcard__buttons">
+        <Link href={`/profile/${id}`} passHref>
+          <button className="devcard__button devcard__button-profile">Profile</button>
+        </Link>
+        <button className="devcard__button devcard__button-follow">Follow</button>
+      </article>
+      <article className="devcard__social"></article>
+    </div>
+  )
+}
+
+export default DevCard
---
+++ components/SkillsTags.jsx
@@ -0,0 +1,34 @@
+import { DrupalIcon, JavaScriptIcon, ReactIcon } from './Icons';
+
+const skills = [
+  {
+    id: 1,
+    name: "React",
+    icon: <ReactIcon className='icon-large primary-blue' />
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    icon: <JavaScriptIcon className='icon-large primary-blue' />
+  },
+  {
+    id: 3,
+    name: "Drupal",
+    icon: <DrupalIcon className='icon-large primary-blue' />
+  },
+];
+
+const SkillsTags = () => {
+  return (
+    <div className="skillstags">
+      {skills.map((skill) => (
+        <div key={skill.id} className="skillstags__box">
+          <div>{skill.icon}</div>
+          <span>{skill.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SkillsTags;
