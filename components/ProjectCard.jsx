4b297c3717a162f66cf4f4352fbf81226f91be92
Arek <arexspeed95@gmail.com>
2022-04-01 16:18:18 +0200

    project front fixes

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,37 +1,24 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import FlagIconSwitcher from './IconSwitcher/FlagIconSwitcher';
 import SkillsIconSwitcher from './IconSwitcher/SkillsIconSwitcher';
-import SocialIconSwitcher from './IconSwitcher/SocialIconSwitcher';
 import { WebsiteIcon } from './Icons/SocialIcons';
-import { Heart } from './Icons/FontIcons';
+import { HeartIcon, HeartOutlineIcon } from './Icons/FontIcons';
 
-function ProjectCard({
-  projectid,
-  title,
-  userid,
-  username,
-  userimage,
-  logo,
-  description,
-  technology,
-  likes
-}) {
-  const [follow, setFollow] = useState(false);
+// eslint-disable-next-line prettier/prettier
+const ProjectCard = ({ projectid, title, userid, username, userimage, logo, link, description, technology, likes }) => {
+  const [like, setLike] = useState(false);
   return (
     <div className="projectcard">
       <div className="projectcard__heading">
-        <div className="projectcard__image"></div>
-        <h2 className="projectcard__title">{title}</h2>
+        <div className="projectcard__image">
+          <img src={logo} alt="" />
+        </div>
+        <div className="projectcard__title">{title}</div>
       </div>
       <p>{description}</p>
       <article className="projectcard__skills">
         {technology.map((skill, i) => (
-          <SkillsIconSwitcher
-            key={i}
-            name={skill}
-            className="icon-medium primary-blue margin-skills"
-          />
+          <SkillsIconSwitcher key={i} name={skill} className="icon-medium primary-blue" />
         ))}
       </article>
 
@@ -43,18 +30,24 @@ function ProjectCard({
       </div>
 
       <div className="projectcard__likes">
-        <Heart className="icon-medium projectcard__likes-heart" />
-        <span className="projectcard__likes-votes">{likes.length}</span>
-
-        <button className="projectcard__btn">
-          <div className="projectcard__btn-wrapper">
-            <WebsiteIcon className="primary-blue icon-medium icon-medium--center" />
-          </div>
-          <div>Go to project</div>
+        <button className="projectcard__likes-btn" onClick={() => setLike(!like)}>
+          {like ? (
+            <HeartOutlineIcon className="icon-medium primary-blue" />
+          ) : (
+            <HeartIcon className="icon-medium primary-blue" />
+          )}
+          <div className="projectcard__likes-count">{likes.length}</div>
         </button>
+
+        <Link href={link} passHref>
+          <a className="projectcard__link" target="_blank">
+            <WebsiteIcon className="icon-medium primary-blue" />
+            <span>Go to project</span>
+          </a>
+        </Link>
       </div>
     </div>
   );
-}
+};
 
 export default ProjectCard;
